53b8af423501a9d6ede1458f0697f424d176e9cc
GineBornales <ginabornales76@gmail.com>
2025-04-05 05:40:30 +0800

    Auto deploy from Termux

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'ginebornales/opensource-casino-v10',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'a6b7ee97a2c99ad271980cdeab00c75c9081a8b7',
+        'reference' => '9c8c0755dd65a5fd1427a4f4377c9e3d77ab43d6',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'ginebornales/opensource-casino-v10' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'a6b7ee97a2c99ad271980cdeab00c75c9081a8b7',
+            'reference' => '9c8c0755dd65a5fd1427a4f4377c9e3d77ab43d6',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
